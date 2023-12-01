6f67f8ba25de885fd819ce00071183dfba87f183
EnspiredjackDev <jackgreenall12@gmail.com>
2023-12-01 23:33:39 +0000

    updating image to latest version of OAI + dalle-3

--- AI_image_bot.py
+++ AI_image_bot.py
@@ -1,15 +1,18 @@
 import discord
-import openai
+from openai import OpenAI
 import requests
 from io import BytesIO
 
 class MyClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
 
     async def on_message(self, message):
-        #print(message.content)
         if message.author == self.user:
             return
 
@@ -26,7 +29,7 @@ class MyClient(discord.Client):
                 image_url = await generate_image(prompt)
                 if image_url:
                     async with message.channel.typing():
-                        image_data = requests.get(image_url).content
+                        image_data = requests.get(image_url).content  
                         image_file = discord.File(BytesIO(image_data), filename="generated_image.png")
                         await message.channel.send(file=image_file)
             except Exception as e:
@@ -34,16 +37,19 @@ class MyClient(discord.Client):
                 await message.channel.send('An error occurred while generating the image. ' + str(e))
 
 async def generate_image(prompt):
-    openai.api_key = "OPENAI-API-KEY"
+    
+    client = OpenAI(api_key='OPENAI-API-KEY')
 
-    response = openai.Image.create(
+    response = client.images.generate(
+        model="dall-e-3",
         prompt=prompt,
-        n=1,
         size="1024x1024",
+        quality="standard",
+        n=1,
     )
-    #print(response)
-    if response and response.get('data') and response['data'][0].get('url'):
-            return response['data'][0]['url']
+    if response.data and len(response.data) > 0:
+        image_url = response.data[0].url
+        return image_url      
     else:
         return None
 
@@ -51,5 +57,5 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-TOKEN = 'DISCORD-TOKEN'
+TOKEN = 'DISCORD-BOT-TOKEN'
 client.run(TOKEN)
--- README.md
+++ README.md
@@ -6,6 +6,8 @@ These bots are very basic and just me messing about.
 ## GPT bot  
 This bot gets every message from the server and replies to it, adding the conversation to the memory, configure how long you want the memory by changing the `MAX_CONVERSATION_LENGTH` to something else. Conversation does not include the system message, it will not be deleted. This is the single message respose version, it will send a message once openai's API has responded.  
 
+`!helpgpt` for help menu thing  
+
 ## GPT bot stream  
 This is the same as above but it will send a message and then continuously edit that message until the response is fully added.  
   
@@ -16,10 +18,11 @@ Use `!setchannel` to set the channel for the AI to talk in otherwise it will not
 Just counts how many times the n-word is said, good for toxic servers i guess. (had to remove the actual words, just add them back)  
 
 ## AI image bot  
-Uses DALL-E 2 image api to generate images when someone says !generateimage prompt (changable) (Not updated to latest version of OAI lib) 
+Uses DALL-E 3 image api to generate images when someone says !generateimage [prompt] (if thats too long, just change it in the script) 
 
 ## Prerequisits
-Make sure you have openai and discord installed via pip
+Make sure you have openai and discord installed via pip for AI related bots, however discord is still required for normal ones.
 `pip install openai discord`
 
 Get a API token at [OpenaAI](https://platform.openai.com)
+Get discord bot + token at [Discord](https://discord.com/developers/applications)
