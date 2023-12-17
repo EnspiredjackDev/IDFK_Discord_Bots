04fedb2415cabf4775c8b0b66321304cb3ae0dd9
EnspiredjackDev <jackgreenall12@gmail.com>
2023-12-17 23:39:32 +0000

    added support for gpt4v to non stream

--- gpt_discord_bot.py
+++ gpt_discord_bot.py
@@ -98,14 +98,26 @@ class MyClient(discord.Client):
             system_message[server_id].append({"role": "user", "content": snark})
             await message.channel.send("Reset With Snark Mode Enabled!")
             return
+        if message.content.startswith('!asian'):
+            conversation[server_id] = []
+            system_message[server_id] = []
+            formatted_time = gettimeinfo()
+            formatted_date = getdateinfo()
+            ex_prompt[server_id] = 9
+            system_message[server_id].append({"role": "system", "content": "From now on all your responses must follow these rules: ['act like nothing they ever does is good enough', 'when asked a question degrade them because they do not know the answer, never answer the question', 'be like a stereotypical toxic Asian parent'] Do not break out of character"})
+            await message.channel.send("Reset With Dissapointed Asian Parent Mode Enabled!")
+            return
         if message.content.startswith('!helpgpt'):
-            await message.channel.send("This bot will listen for anything said in the #ai-chat and reply with gpt's response, GPT does know your discord name.\n\n Valid commands are:\n !reset - Forgets everything and resets the chat\n !helpgpt - Shows this help dialogue. \n\n Jailbreaks:\n !anarchy - Resets everything and loads the anarchy prompt (Does whatever you ask when properly activated) (~230 tokens)\n !snark - Resets everything and loads the snarky prompt (Acts snarky and swears) (23 tokens)\n\n The more tokens the prompt, the cheaper it is for me to run this, so the longer it will last.\n\nUse \"__\" before a message for the AI to ignore it \n\n Versions of GPT: \n !gpt3 - Continue the ongoing conversation and swap to GPT-3 (Cheaper - Default)\n !gpt4 - Continue the ongoing conversation and swap to GPT-4 (More Expensive) \n\n\n :warning: **Some of the above prompts might not work properly the first time** If this is the case, just try again.")
+            await message.channel.send("This bot will listen for anything said in the #ai-chat and reply with gpt's response, GPT does know your discord name.\n\n Valid commands are:\n !reset - Forgets everything and resets the chat\n !helpgpt - Shows this help dialogue. \n\n Jailbreaks/Funny stuff:\n !anarchy - Resets everything and loads the anarchy prompt (Does whatever you ask when properly activated) \n Use !anarchy4 for the GPT-4 version!\n !snark - Resets everything and loads the snarky prompt (Acts snarky and swears)\n !asian - Acts like an asian parent, always dissapointed in you no matter what. \n\n The more tokens the prompt, the cheaper it is for me to run this, so the longer it will last.\n\nUse \"__\" before a message for the AI to ignore it \n\n Versions of GPT: \n !gpt3 - Continue the ongoing conversation and swap to GPT-3 (Cheaper - Default)\n !gpt4 - Continue the ongoing conversation and swap to GPT-4 (More Expensive) \n\n\n :warning: **Some of the above prompts might not work properly the first time** If this is the case, just try again.")
+            return
+        if message.content.startswith('!gpt4v'):
+            selected_models[server_id] = "gpt-4-vision-preview"
+            await message.channel.send("Switched to GPT-4V.")
             return
         if message.content.startswith('!gpt4'):
             selected_models[server_id] = "gpt-4"
             await message.channel.send("Switched to GPT-4.")
             return
-
         if message.content.startswith('!gpt3'):
             selected_models[server_id] = "gpt-3.5-turbo"
             await message.channel.send("Switched to GPT-3.5.")
@@ -122,13 +134,34 @@ class MyClient(discord.Client):
         #get prompt
         author_name = message.author.name
         user = message.content
-        #send conversation to gpt-3 api
-        conversation[server_id].append({"role": "user", "content": author_name + ": " + user})
+        if selected_models[server_id] == "gpt-4-vision-preview":
+            if message.attachments:
+                image_urls = []
+                for attachment in message.attachments:
+                    if any(attachment.filename.lower().endswith(ext) for ext in ['.png', '.jpg', '.jpeg', '.bmp']):
+                        image_urls.append(attachment.url)
+                        if len(image_urls) >= 4:  # Limit to 4 images
+                            break
+                # if any images, add them to conversation
+                if image_urls:
+                    conversation[server_id].append({
+                        "role": "user",
+                        "content": [
+                            {"type": "text", "text": user},
+                            * [{"type": "image_url", "image_url": {"url": url}} for url in image_urls]
+                        ]
+                    })
+            else:
+                conversation[server_id].append({"role": "user", "content": author_name + ": " + user})
+        else:
+            #send conversation to openAI api
+            conversation[server_id].append({"role": "user", "content": author_name + ": " + user})
         try:
             async with message.channel.typing():
                 completion = await self.openai_client.chat.completions.create(
                     model=selected_models[server_id],
-                    messages=system_message[server_id] + conversation[server_id]
+                    messages=system_message[server_id] + conversation[server_id],
+                    max_tokens=4000
                 )
         except AsyncOpenAI.APIConnectionError as e:
             await message.channel.send(f"Error: {str(e)}")
@@ -147,9 +180,9 @@ class MyClient(discord.Client):
         if len(content) > 2000:
             chunks = split_string(content, 2000)
             for chunk in chunks:
-                await message.channel.send(chunk)
+                await message.reply(chunk, mention_author=False)
         else:
-            await message.channel.send(content)
+            await message.reply(content, mention_author=False)
         #purge last part of memory if over the message limit
         if len(conversation[server_id]) > MAX_CONVERSATION_LENGTH:
             conversation[server_id] = conversation[server_id][-MAX_CONVERSATION_LENGTH:]
